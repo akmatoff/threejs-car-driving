766e6f85ac24452272b5215c5f0368d8fcf9ada9
Azim Akmatov <49513936+akmatoff@users.noreply.github.com>
2021-03-17 17:15:38 +0600

    camera movement fixed

--- src/Scene.js
+++ src/Scene.js
@@ -8,8 +8,8 @@ import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass";
 import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer";
 import Car from "./objects/Car";
 import Ground from "./objects/Ground";
-import { InputManager } from './InputManager'
-import { SkyShader } from './shaders/skyShader'
+import { InputManager } from "./InputManager";
+import { SkyShader } from "./shaders/skyShader";
 
 export default class Scene {
   constructor() {
@@ -26,11 +26,11 @@ export default class Scene {
 
     window.addEventListener("mousemove", () => {
       this.onMouseMove();
-    })
+    });
 
     window.addEventListener("mousedown", () => {
       this.onMouseDown();
-    })
+    });
   }
 
   init() {
@@ -38,7 +38,7 @@ export default class Scene {
     this.world = new CANNON.World();
     this.world.broadphase = new CANNON.SAPBroadphase(this.world); // Change the collision detection method
     this.world.gravity.set(0, -9.82, 0);
-    this.world.defaultContactMaterial.friction = 0
+    this.world.defaultContactMaterial.friction = 0;
 
     // Clock
     this.clock = new THREE.Clock();
@@ -56,9 +56,9 @@ export default class Scene {
     this.addObjects();
     this.setRenderer();
 
-    this.inputManger = new InputManager(this.car.raycastVehicle)
+    this.inputManger = new InputManager(this.car.raycastVehicle);
 
-    this.render()
+    this.render();
   }
 
   setRenderer() {
@@ -75,20 +75,27 @@ export default class Scene {
     this.renderer.setAnimationLoop(() => {
       this.render();
     }); // Render loop
-    
+
     // Passes
     this.renderPass = new RenderPass(this.scene, this.camera);
-    this.unrealBloomPass = new UnrealBloomPass(new THREE.Vector2(window.innerWidth, window.innerHeight), 0.1, 1.2, 0.5)
+    this.unrealBloomPass = new UnrealBloomPass(
+      new THREE.Vector2(window.innerWidth, window.innerHeight),
+      0.1,
+      1.2,
+      0.5
+    );
 
     this.fxaaPass = new ShaderPass(FXAAShader);
 
     const pixelRatio = this.renderer.getPixelRatio();
 
-    this.fxaaPass.material.uniforms[ 'resolution' ].value.x = 1 / ( window.innerWidth * pixelRatio );
-    this.fxaaPass.material.uniforms[ 'resolution' ].value.y = 1 / ( window.innerHeight * pixelRatio );
+    this.fxaaPass.material.uniforms["resolution"].value.x =
+      1 / (window.innerWidth * pixelRatio);
+    this.fxaaPass.material.uniforms["resolution"].value.y =
+      1 / (window.innerHeight * pixelRatio);
 
     this.gammaCorrectionPass = new ShaderPass(GammaCorrectionShader);
-    this.skyShaderPass = new ShaderPass(SkyShader)
+    this.skyShaderPass = new ShaderPass(SkyShader);
 
     // Composer
     this.composer = new EffectComposer(this.renderer);
@@ -111,9 +118,9 @@ export default class Scene {
 
     this.camera.position.set(0, 5, -80);
 
-    this.currentLookPos = new THREE.Vector3()
-    this.currentCamPos = new THREE.Vector3()
-    this.cameraOffset = new THREE.Vector3()
+    this.currentLookPos = new THREE.Vector3();
+    this.currentCamPos = new THREE.Vector3();
+    this.cameraOffset = new THREE.Vector3();
 
     this.rotationSpeed = 0.05;
 
@@ -129,8 +136,8 @@ export default class Scene {
     // Directional light
     this.dirLight = new THREE.DirectionalLight(0xffe5c9, 0.5);
     this.dirLight.position.set(0, 200, 200);
-    this.dirLight.target.position.set(0, 0, -100)
-    this.dirLight.castShadow = true;
+    this.dirLight.target.position.set(0, 0, -100);
+    // this.dirLight.castShadow = true;
     this.dirLight.shadow.mapSize.width = 2048;
     this.dirLight.shadow.mapSize.height = 2048;
     this.dirLight.shadow.bias = -0.0001;
@@ -140,11 +147,10 @@ export default class Scene {
     this.dirLight.shadow.camera.top = 500;
     this.dirLight.shadow.camera.far = 1000;
     this.dirLight.shadow.camera.near = 0.5;
-    this.scene.add(this.dirLight)
+    this.scene.add(this.dirLight);
 
-    this.dirLightHelper = new THREE.DirectionalLightHelper(this.dirLight)
+    this.dirLightHelper = new THREE.DirectionalLightHelper(this.dirLight);
     // this.scene.add(this.dirLightHelper)
-
   }
 
   addObjects() {
@@ -152,18 +158,30 @@ export default class Scene {
     this.ground = new Ground(this.scene, this.world);
 
     // New instance of the car
-    this.car = new Car(this.scene, this.world, new CANNON.Vec3(0, 16, 0), 0xe04000, {
-      materials: [this.ground.groundMaterial]
-    });
+    this.car = new Car(
+      this.scene,
+      this.world,
+      new CANNON.Vec3(0, 16, 0),
+      0xe04000,
+      {
+        materials: [this.ground.groundMaterial]
+      }
+    );
 
-    this.car2 = new Car(this.scene, this.world, new CANNON.Vec3(-10, 16, 0), 0x1b1b1b, {
-      materials: [this.ground.groundMaterial]
-    })
+    this.car2 = new Car(
+      this.scene,
+      this.world,
+      new CANNON.Vec3(-10, 16, 0),
+      0x1b1b1b,
+      {
+        materials: [this.ground.groundMaterial]
+      }
+    );
   }
 
   updatePhysics() {
-    this.car.updatePhysics()
-    this.car2.updatePhysics()
+    this.car.updatePhysics();
+    this.car2.updatePhysics();
     this.world.step(this.timeStep);
   }
 
@@ -174,28 +192,27 @@ export default class Scene {
       this.car.carBody.position.y,
       this.car.carBody.position.z
     );
-    
+
     // Make smoother following of the position
-    this.currentLookPos.lerp(this.lookPos, 0.1)
-    
-    // initial position of the camera 
+    this.currentLookPos.lerp(this.lookPos, 0.8);
+
+    // initial position of the camera
     this.camPos = new THREE.Vector3(
       this.car.carBody.position.x,
       this.car.carBody.position.y + 3,
       this.car.carBody.position.z + 18
-    )
-    
+    );
+
     // Add camera offset before adding inital camera position, which is needed to add additional position to rotate the camera
-    this.currentCamPos.lerp(this.camPos.add(this.cameraOffset), 0.1)
-    
+    this.currentCamPos.lerp(this.camPos.add(this.cameraOffset), 1);
+
     // Set the camera's position to the current camera position
     this.camera.position.copy(this.currentCamPos);
-    
-    this.camera.lookAt(this.currentLookPos);
-    
-    // Prevent the camera from going lower than ground
-    if (this.camera.position.y < 2) this.camera.position.y = 2
 
+    this.camera.lookAt(this.currentLookPos);
+
+    // Prevent the camera from going lower than ground
+    if (this.camera.position.y < 2) this.camera.position.y = 2;
   }
 
   render() {
@@ -203,10 +220,10 @@ export default class Scene {
     this.updateCamera();
 
     this.raycaster.setFromCamera(this.mouse, this.camera);
-    
+
     // Objects update
-    this.ground.groundCamera.update(this.renderer, this.scene)
-    this.car.carCamera.update(this.renderer, this.scene)
+    this.ground.groundCamera.update(this.renderer, this.scene);
+    this.car.carCamera.update(this.renderer, this.scene);
 
     this.composer.render(this.clock.getDelta());
   }
@@ -215,26 +232,24 @@ export default class Scene {
     this.camera.aspect = window.innerWidth / window.innerHeight;
     this.camera.updateProjectionMatrix();
     this.renderer.setSize(window.innerWidth, window.innerHeight);
-    
+
     const pixelRatio = this.renderer.getPixelRatio();
 
-    this.fxaaPass.material.uniforms[ 'resolution' ].value.x = 1 / ( window.innerWidth * pixelRatio );
-    this.fxaaPass.material.uniforms[ 'resolution' ].value.y = 1 / ( window.innerHeight * pixelRatio );
+    this.fxaaPass.material.uniforms["resolution"].value.x =
+      1 / (window.innerWidth * pixelRatio);
+    this.fxaaPass.material.uniforms["resolution"].value.y =
+      1 / (window.innerHeight * pixelRatio);
   }
 
   onMouseMove() {
-
     // If mouse is locked
     if (document.pointerLockElement === this.renderer.domElement) {
       const e = window.event;
-
-    } 
-;
-    
+    }
   }
 
   onMouseDown() {
-    // Lock the mouse on click 
+    // Lock the mouse on click
     this.renderer.domElement.requestPointerLock();
   }
 }
